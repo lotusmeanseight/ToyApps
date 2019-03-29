dee411af9f031fc1d1dd949c22f63081d4be8e93
lotusmeanseight <g.goering@ostfalia.de>
2019-03-29 12:37:10 +0100

    hateoas extension still needs work

--- hateoas-extended/src/main/java/com/github/lotusmeanseight/entity/controller/WineController.java
+++ hateoas-extended/src/main/java/com/github/lotusmeanseight/entity/controller/WineController.java
@@ -5,34 +5,49 @@ import com.github.lotusmeanseight.entity.repository.WineRepository;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 
 @RestController
 public class WineController {
 
     private final WineRepository wineRepository;
+    private Class<WineController> controllerClass;
 
     public WineController(WineRepository wineRepository){
         this.wineRepository = wineRepository;
+        this.controllerClass = WineController.class;
     }
 
     @GetMapping("/wines/{id}")
-    public EntityModel<Wine> findOne(@PathVariable Integer id){
-        Class<WineController> controllerClass = WineController.class;
-        var selfLink = linkTo(methodOn(controllerClass).findOne(id)).withSelfRel();
-        var wine = wineRepository.findById(id).get();
-        return new EntityModel<>(wine, selfLink);
+    public ResponseEntity<EntityModel<Wine>> findOne(@PathVariable Integer id){
+        return wineRepository.findById(id)
+                .map(wine -> new EntityModel<>(wine,linkTo(methodOn(controllerClass).findOne(id)).withSelfRel()
+                        .andAffordance(afford(linkTo(methodOn(controllerClass).findAll())))))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/wines")
     public ResponseEntity<CollectionModel<EntityModel<Wine>>> findAll(){
-        return ResponseEntity.ok(CollectionModel.wrap(wineRepository.findAll()));
+        List<EntityModel<Wine>> wines = StreamSupport.stream(wineRepository.findAll().spliterator(), false)
+                .map(wine -> new EntityModel<>(wine, linkTo(methodOn(controllerClass).findOne(wine.getId()))
+                        .withSelfRel().andAffordance(afford(linkTo(methodOn(controllerClass).createWine(null))))))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(new CollectionModel<>(wines));
+    }
+
+    @PostMapping("/wines")
+    public ResponseEntity<Wine> createWine(@RequestBody Wine newWine){
+        //TODO
+        Wine wine = wineRepository.save(newWine);
+        return null;
     }
 
 }
